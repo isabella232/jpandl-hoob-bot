c881e8ee455047d53348b6292527bb4cf1677f49
Jacob Pandl <jpandl19@gmail.com>
2016-07-29 14:37:10 -0700

    added google 📆, oauth 🔒, redis 💿

--- slack_bot.js
+++ slack_bot.js
@@ -1,245 +1,157 @@
-const Botkit = require('botkit');
-const NATURAL = require('natural');
-const CHRONO  = require('chrono-node');
+const Botkit = require('botkit')
+const NATURAL = require('natural')
+const CHRONO  = require('chrono-node')
+const CALENDAR = require('./google_calendar_api')
+const env = require('./.env.js')
+
+// Programmatically use appropriate process environment variables
+try {
+  require('./.env.js');
+} catch (e) {
+  if (e.code === 'MODULE_NOT_FOUND') {
+    console.log('Not using environment variables from env.js');
+  }
+}
+var port = process.env.PORT || process.env.port;
+
+if (!process.env.clientId || !process.env.clientSecret || !port) {
+  console.log('Error: Specify clientId clientSecret and port in environment');
+  process.exit(1);
+}
+
+//Use REDIS store (MAKE SURE REDIS IS RUNNING- use redis-server in terminal)
+var Redis_Store = require('./redis_storage.js');
+var redis_url = "redis://127.0.0.1:6379"
+var redis_store = new Redis_Store({url: redis_url});
+
 
 //Labels
 const HELLO = 'hello'
 const WHIP = 'whip'
 const TICKET = 'ticket'
-
+const TODAY = 'today'
+const RELEASE = 'release'
+const RELEASE_MANAGER = 'release_manager'
+const SPRINT = 'sprint'
 
 // Initiate NLP classifier
-const classifier = new NATURAL.BayesClassifier();
+const classifier = new NATURAL.BayesClassifier()
 
 // Train classifier
 classifier.addDocument('Hey', HELLO)
-classifier.addDocument('Who is the next whip', WHIP);
-classifier.addDocument('Who is on ticket duty', TICKET);
-classifier.addDocument('What is happening today', 'today');
-classifier.addDocument('When is next release', 'release');
-classifier.addDocument('Who is the next release manager', 'release manager');
-classifier.addDocument('When does this sprint end', 'sprint');
+classifier.addDocument('Who is the next whip', WHIP)
+classifier.addDocument('Who is on ticket duty', TICKET)
+classifier.addDocument('What is happening today', TODAY)
+classifier.addDocument('When is next release', RELEASE)
+classifier.addDocument('Who is the next release manager', RELEASE_MANAGER)
+classifier.addDocument('When does this sprint end', SPRINT)
 
-classifier.train();
+classifier.train()
 
 function getLabelFunctions(label){
   const labelFunctions = {
     'hello': function (bot, message) {
-      bot.reply(message, `label: ${HELLO}`);
+      console.log(CALENDAR.startAuth)
+      CALENDAR.startAuth()
+      bot.reply(message, "Searching calendar")
     },
     'whip': function (bot, message) {
-      bot.reply(message, `label: ${WHIP}`);
+      bot.reply(message, `label: ${WHIP}`)
     },
     'ticket': function (bot, message) {
-      bot.reply(message, `label: ${TICKET}`);
+      bot.reply(message, `label: ${TICKET}`)
     }
   }
-
-  return labelFunctions[label];
+  return labelFunctions[label]
 }
 
-
 const controller = Botkit.slackbot({
-    debug: false,
-    interactive_replies: true,
-    json_file_store: './bot_storage'
-})
-
-const bot = controller.spawn({
-    token: 'xoxb-56251165760-Gbd3f8GHicYZCDa4PZrB77LT'
-})
-
-bot.startRTM(function(err,bot,payload){
-  if(err){
-    throw new Error('Could not connect to Slack')
+    debug: true,
+    storage: redis_store
+}).configureSlackApp(
+  {
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    scopes: ['bot'],
   }
-})
+)
 
-controller.on('direct_message,direct_mention, mention', function(bot, message){
-  // Run message through classifier
-  const match = classifier.getClassifications(message.text);
-  console.log("\n", JSON.stringify(match[0]));
+controller.setupWebserver(port,function(err,webserver) {
 
-  const {label} = match[0];
+  webserver.get('/',function(req,res) {
+    res.sendFile('index.html', {root: __dirname});
+  });
 
-  getLabelFunctions(label)(bot, message);
+  controller.createWebhookEndpoints(controller.webserver);
 
-  // Sort match
-  // const sorted = match.sort.apply(match, match.map(o => o.value));
-
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
 
+// just a simple way to make sure we don't
+// connect to the RTM twice for the same team
+var _bots = {}
+function trackBot(bot) {
+  _bots[bot.config.token] = bot
+}
 
-// controller.hears(['who\'s the whip', 'whos the whip', 'whos whip'], 'direct_message,direct_mention,mention',
-//   function(bot, message){
-//     var team = message.team
-//     controller.storage.teams.get(team, function(err, team_data) {
-//       if(err || !team_data){
-//         saveTeam(team)
-//         bot.reply(message, 'There is no whip!');
-//       }
-//       else if(team_data.whip && team_data.whip.name){
-//           bot.reply(message, 'The whip is ' + team_data.whip.name + '!!');
-//       }
-//       else {
-//           bot.reply(message, 'There is no whip!');
-//       }
-//     })
-//   })
-//
-// controller.hears(['set the whip', 'set whip', 'pick a whip', 'pick whip'], 'direct_message,direct_mention,mention,ambient',
-//   function(bot, message) {
-//     var team = {id: message.team};
-//     controller.storage.teams.get(team.id, function(err, foundTeam){
-//       console.log("Team: " + foundTeam)
-//     })
-//     team.whip = {name: "Baldvin"}
-//     saveTeam(team.id, team.name, team.whip)
-//   })
-//
-// controller.hears(['hello', 'waddup', 'yo'], 'direct_message,direct_mention,mention', function(bot, message) {
-//   console.log(message)
-//     bot.api.reactions.add({
-//         timestamp: message.ts,
-//         channel: message.channel,
-//         name: 'robot_face',
-//     }, function(err, res) {
-//         if (err) {
-//             bot.botkit.log('Failed to add emoji reaction :(', err);
-//         }
-//     })
-//
-//
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message, 'Hello ' + user.name + '!!');
-//         } else {
-//             bot.reply(message, 'Hello.');
-//         }
-//     })
-// })
-//
-// controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-//     var name = message.match[1];
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (!user) {
-//             user = {
-//                 id: message.user,
-//             };
-//         }
-//         user.name = name;
-//         controller.storage.users.save(user, function(err, id) {
-//             bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-//         });
-//     });
-// });
-//
-// controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-//
-//     controller.storage.users.get(message.user, function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message, 'Your name is ' + user.name);
-//         } else {
-//             bot.startConversation(message, function(err, convo) {
-//                 if (!err) {
-//                     convo.say('I do not know your name yet!');
-//                     convo.ask('What should I call you?', function(response, convo) {
-//                         convo.ask('You want me to call you `' + response.text + '`?', [
-//                             {
-//                                 pattern: 'yes',
-//                                 callback: function(response, convo) {
-//                                     // since no further messages are queued after this,
-//                                     // the conversation will end naturally with status == 'completed'
-//                                     convo.next();
-//                                 }
-//                             },
-//                             {
-//                                 pattern: 'no',
-//                                 callback: function(response, convo) {
-//                                     // stop the conversation. this will cause it to end with status == 'stopped'
-//                                     convo.stop();
-//                                 }
-//                             },
-//                             {
-//                                 default: true,
-//                                 callback: function(response, convo) {
-//                                     convo.repeat();
-//                                     convo.next();
-//                                 }
-//                             }
-//                         ]);
-//
-//                         convo.next();
-//
-//                     }, {'key': 'nickname'}); // store the results in a field called nickname
-//
-//                     convo.on('end', function(convo) {
-//                         if (convo.status == 'completed') {
-//                             bot.reply(message, 'OK! I will update my dossier...');
-//
-//                             controller.storage.users.get(message.user, function(err, user) {
-//                                 if (!user) {
-//                                     user = {
-//                                         id: message.user,
-//                                     };
-//                                 }
-//                                 user.name = convo.extractResponse('nickname');
-//                                 controller.storage.users.save(user, function(err, id) {
-//                                     bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-//                                 });
-//                             });
-//
-//                         } else {
-//                             // this happens if the conversation ended prematurely for some reason
-//                             bot.reply(message, 'OK, nevermind!');
-//                         }
-//                     });
-//                 }
-//             });
-//         }
-//     });
-// });
-//
-//
-// controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-//
-//     bot.startConversation(message, function(err, convo) {
-//
-//         convo.ask('Are you sure you want me to shutdown?', [
-//             {
-//                 pattern: bot.utterances.yes,
-//                 callback: function(response, convo) {
-//                     convo.say('Bye!');
-//                     convo.next();
-//                     setTimeout(function() {
-//                         process.exit();
-//                     }, 3000);
-//                 }
-//             },
-//         {
-//             pattern: bot.utterances.no,
-//             default: true,
-//             callback: function(response, convo) {
-//                 convo.say('*Phew!*');
-//                 convo.next();
-//             }
-//         }
-//         ]);
-//     });
-// });
+controller.on('create_bot',function(bot,config) {
+
+  if (_bots[bot.config.token]) {
+    // already online! do nothing.
+  } else {
+    bot.startRTM(function(err) {
+
+      if (!err) {
+        trackBot(bot)
+      }
+
+      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
+        if (err) {
+          console.log(err)
+        } else {
+          convo.say('I am a bot that has just joined your team')
+          convo.say('You must now /invite me to a channel so that I can be of use!')
+        }
+      })
+
+    })
+  }
+
+})
+
+//Handle slack bot mentions
+controller.on('direct_message,direct_mention, mention', function(bot, message){
+  // Run message through classifier
+  const match = classifier.getClassifications(message.text)
+  console.log("\n", JSON.stringify(match[0]))
+
+  const label = match[0].label
+
+  //TODO: Need code to determine if match is "good enough"
+
+  //Match the detected label with the label's corresponding function
+  getLabelFunctions(label)(bot, message)
+
+})
 
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
     'direct_message,direct_mention,mention', function(bot, message) {
 
-        var uptime = formatUptime(process.uptime());
+        var uptime = formatUptime(process.uptime())
 
         bot.reply(message,
             ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + '.');
+             '>. I have been running for ' + uptime + '.')
 
-    });
+    })
 
 function getTeamById(teamId){
   console.log("Team ID: " + teamId)
@@ -262,25 +174,25 @@ function saveTeam(teamID, name, whip){
   console.log("Saving team: " + newTeam)
   controller.storage.teams.save(newTeam, function(err) {
       if(err){
-        bot.reply(message, 'Sorry I can\'t remember your team info now. Leave me alone');
+        bot.reply(message, 'Sorry I can\'t remember your team info now. Leave me alone')
       }
   })
 }
 
 function formatUptime(uptime) {
-    var unit = 'second';
+    var unit = 'second'
     if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
+        uptime = uptime / 60
+        unit = 'minute'
     }
     if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
+        uptime = uptime / 60
+        unit = 'hour'
     }
     if (uptime != 1) {
-        unit = unit + 's';
+        unit = unit + 's'
     }
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
+    uptime = uptime + ' ' + unit
+    return uptime
 }
